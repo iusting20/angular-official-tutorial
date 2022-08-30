ce33d63ffca64c8c703739254c9a93abc49761e5
Iustin Ghergu <iusting20@gmail.com>
2022-08-30 10:13:20 +0100

    created the Cart component and made it functional

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  constructor(private cart: CartService) {}
 
-  constructor() { }
+  items = this.cart.getItems();
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
